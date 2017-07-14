1aad2a818e853563931b3ea6c86ce59dd5d59792
SadleyButton <brad.kh.sutton@gmail.com>
2017-07-14 01:21:21 -0400

    Added IHasKarma interface

---
+++ COMP123-S2017-Lesson9B/IHasKarma.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Bradley Sutton
+ * Date: July 14, 2017
+ * Description: Interface to define Karma Property that must be implimented in any class that subscribes
+ * Version: 0.1 - Created Interface (IHasKarma)
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+    /// <summary>
+    /// This is the IHasKarama Interface
+    /// </summary>
+    public interface IHasKarma
+    {
+        // PUBLIC PROPERTIES
+        int Karma { get; set; }
+    }
+}
