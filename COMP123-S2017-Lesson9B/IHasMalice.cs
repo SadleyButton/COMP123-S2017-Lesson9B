c825863907605909c5e5112a8f8f0a61ef815c36
SadleyButton <brad.kh.sutton@gmail.com>
2017-07-14 01:57:02 -0400

    Created interface IHasMalice

---
+++ COMP123-S2017-Lesson9B/IHasMalice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Bradley Sutton
+ * Date: July 14, 2017
+ * Description: Interface to define Malice Property that must be implimented in any class that subscribes
+ * Version: 0.1 - Created Interface (IHasMalice)
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+    /// <summary>
+    /// This is the IHasKarama Interface
+    /// </summary>
+    public interface IHasMalice
+    {
+        // PUBLIC PROPERTIES
+        int Malice { get; set; }
+    }
+}
