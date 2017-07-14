e91d82df14cff1403d2735ff2f09d4481cd64afa
SadleyButton <brad.kh.sutton@gmail.com>
2017-07-13 23:52:01 -0400

    Added Power class

---
+++ COMP123-S2017-Lesson9B/Power.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Bradley Sutton
+ * Date: July 13, 2017
+ * Description: This is the power class that names and ranks the powers.
+ * Version: 0.1 - Created the Power class
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+    /// <summary>
+    /// This is the power class which will be used as a Data Type
+    /// </summary>
+    public class Power
+    {
+        // PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public int Rank { get; set; }
+
+        // CONSTRUCTORS -----------------------------------
+
+    }
+}
