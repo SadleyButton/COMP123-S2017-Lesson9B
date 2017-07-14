17c8e1e4bd08ec47a418471a80e3a60faef475b9
SadleyButton <brad.kh.sutton@gmail.com>
2017-07-14 01:08:01 -0400

    Used the ToString method of the SuperHuman class in the Driver Class

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -8,8 +8,7 @@ using System.Threading.Tasks;
  * Name: Bradley Sutton
  * Date: July 13, 2017
  * Description: Lesson 9
- * Version: 0.4 - Added another power to the SuperHuman object
- *              - Displayed all powers in the Powers List
+ * Version: 0.5 - Used the ToString method of the SuperHuman class
  */
 
 namespace COMP123_S2017_Lesson9B
@@ -22,7 +21,9 @@ namespace COMP123_S2017_Lesson9B
             //superHuman.Powers.Add(new Power("Spider Climbing", 50));
             superHuman.AddPower("Spider Climbing", 50);
             superHuman.AddPower("Webcasting", 40);
-            superHuman.DisplayPowers();
+            superHuman.AddPower("Spider Sense", 75);
+            //superHuman.DisplayPowers();
+            Console.WriteLine(superHuman.ToString());
         }
     }
 }
