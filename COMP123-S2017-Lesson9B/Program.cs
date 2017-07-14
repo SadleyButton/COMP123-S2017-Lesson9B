da56d79816e2e94034607a767985e7638bbc1de4
SadleyButton <brad.kh.sutton@gmail.com>
2017-07-13 22:47:57 -0400

    Modified Program.cs

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Name: Bradley Sutton
+ * Date: July 13, 2017
+ * Description: Lesson 9
+ * Version: 0.1 - Created for COMP123
+ */
+
 namespace COMP123_S2017_Lesson9B
 {
     class Program
